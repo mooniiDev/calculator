89648d0daea0a4ed7857f18885564239d4223ba0
mooniiDev <moonii@riseup.net>
2020-12-02 12:39:09 +0200

    Update style, add calculations, clear, back and display number functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,47 @@
- 
+const symbols = document.querySelectorAll('.symbol');
+const display = document.querySelector('#display');
+let number1 = '';
+
+// CALCULATIONS
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(arr) {
+  let answer = 1;
+  for (let i = 0; i < arr.length; i++) {
+    answer = answer * arr[i];
+  }
+  return answer;
+}
+function divide(a, b) {
+  return a / b;
+}
+function power(a, b) {
+  let i = 0;
+  let answer = 1;
+  while (i < b) {
+    answer = answer * a;
+    i++;
+  }
+  return answer;
+}
+
+function operate() {
+  symbols.forEach((button) => {
+    button.addEventListener('click', () => {
+      if (button.id === 'clear') {
+        display.textContent = ''
+      }
+      else if (button.id === 'back') {
+        display.textContent = display.textContent.slice(0, -1);
+      } else if (button.classList.contains('number')) {
+        display.textContent = display.textContent + button.textContent;
+      }
+      number1 = display.textContent;
+    })
+  })
+}
+operate();
